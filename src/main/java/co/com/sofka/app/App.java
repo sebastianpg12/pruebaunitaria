8012cba74c87311353779461540b01dc48cbc6a6
Sebastián Pulgarín <pulgaringomezsebastian@gmail.com>
2021-02-18 16:40:29 -0500

    cambios en app

--- src/main/java/co/com/sofka/app/App.java
+++ src/main/java/co/com/sofka/app/App.java
@@ -1,13 +1,42 @@
 package co.com.sofka.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private static final Logger logger = LoggerFactory.getLogger(App.class );
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in ) );
+
+        System.out.println( "Enter number 1: " );
+        String textNumber1 = bufferedReader.readLine();
+
+        System.out.println( "Enter number 2: " );
+        String textNumber2 = bufferedReader.readLine();
+
+        Long number1 = Long.valueOf( textNumber1 );
+        Long number2 = Long.valueOf( textNumber2 );
+
+        BasicCalculator calculator = new BasicCalculator();
+        Long resultSum = calculator.sum( number1, number2 );
+        Long resultRes = calculator.res( number1, number2 );
+        Long resultMul = calculator.mul( number1, number2 );
+        Long resultDiv = calculator.div( number1, number2 );
+
+        System.out.println( number1 + " + " + number2 + " = " + resultSum );
+        System.out.println( number1 + " - " + number2 + " = " + resultRes );
+        System.out.println( number1 + " * " + number2 + " = " + resultMul );
+        System.out.println( number1 + " / " + number2 + " = " + resultDiv );
     }
 }
